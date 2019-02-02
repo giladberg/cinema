01cbde223b963acd847528b7daeaa68a56e04b0e
gilad bergmann <giladberg@gmail.com>
2019-02-02 21:08:23 +0200

    last change

--- cinema/src/components/search-bar.js
+++ cinema/src/components/search-bar.js
@@ -47,7 +47,7 @@ class SearchBar extends Component {
        <form   className="input-group">
         <input placeholder="Choose a name of your favorite movie" className="form-control-md search-bar  col-sm-7 ml-9 border border-warning" value={this.state.movie} onChange={this.onInputChange}/>
         <span className="input-group-btn  mr-4">
-        <input type="button" value="Search"  className="btn-md btn-warning mr-5 text-white " onClick={this.searchMovie}/>
+        <input type="button" value="Search"  className="btn-md btn-warning text-white col-sm-12 " onClick={this.searchMovie}/>
         </span>
         </form>
         {popup}
